31856b1bb0ef49cdda7490bf17fe5607f000bb35
Jonathan Alicea <jonathan@boldideas.net>
2019-06-28 15:55:45 -0400

    Hotel Management App => remove comments and cleaned up code.

--- hotel.py
+++ hotel.py
@@ -1,3 +1,10 @@
+#####Assignment Parameters#####
+## Display a menu asking whether to check in or check out.
+## Prompt the user for a floor number, then a room number.
+## If checking in, ask for the number of occupants and what their names are.
+## If checking out, remove the occupants from that room.
+## not allow anyone to check into a room that is already occupied.
+## Do not allow checking out of a room that isn't occupied.
 hotel = {
  '1': {
    '101': ['George Jefferson', 'Wheezy Jefferson'],
@@ -9,40 +16,6 @@ hotel = {
    '333': ['Neo', 'Trinity', 'Morpheus']
  }
 }
-#print(hotel["1"]["101"][1])
-
-## Display a menu asking whether to check in or check out.
-## Prompt the user for a floor number, then a room number.
-## If checking in, ask for the number of occupants and what their names are.
-## If checking out, remove the occupants from that room.
-## not allow anyone to check into a room that is already occupied.
-## Do not allow checking out of a room that isn't occupied.
-
-
-#inputguestfloor = input("What floor are you on? >> ")
-#inputguestroom = input("what is your room number? >> ")
-#multiguest = True
-
-
-#prompt
-#inputcheckinout = input("Do you want to check IN or Out? >> ")
-#checkin
-# if inputcheckinout.lower() == "in":
-#     guestname = input("what is your name? >> ")
-#     numbguest = input("how many people are in your party? >> ")
-#     if int(numbguest) > 1:  
-#         input("what are the names in your party? >>")
-#checkout 
-# else: 
-#     inputguestfloor = input("What floor are you on? >> ")
-#     inputguestroom = input("what is your room number? >> ")
-#     if hotel.get(inputguestfloor) and hotel.get(inputguestfloor).get(inputguestroom) != None:
-#         del hotel[inputguestfloor][inputguestroom]
-#     else:
-#         print("that is an empty room")
-
-
-
 
 
 def checkincheck():
